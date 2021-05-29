c1820e023891159a409f66b2119bf7a2234d785b
KyeongminAhn <yeezus_92@naver.com>
2021-05-30 04:45:26 +0900

    0530 주석 일부 수정

--- Spring20210521/src/main/java/com/icia/ex/dao/ExDAO.java
+++ Spring20210521/src/main/java/com/icia/ex/dao/ExDAO.java
@@ -10,8 +10,8 @@ import com.icia.ex.dto.ExDTO;
 
 @Repository
 public class ExDAO {
-	// DAO: Data Access Object
-	// DTO: Data Transfer Object
+	// DAO: Data Access Object (DB에 접근해 데이터를 조회, 조작하는 객체)
+	// DTO: Data Transfer Object (클래스 간 데이터 교환을 위한 매개체)
 	
 	@Autowired
 	private SqlSessionTemplate sql;
